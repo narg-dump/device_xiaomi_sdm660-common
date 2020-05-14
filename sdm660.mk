9876efd7bdd4c60de4315a01cb79b1dd51553383
wangling <wangling@codeaurora.org>
2020-05-14 14:09:16 +0800

    Add RRO Target flag for falcon_64
    Change-Id: I3f69596852c16332aa1a14fc29cb399d4be999da
    Signed-off-by: narjirolu125 <narjirolu@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -405,6 +405,12 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images
 
+# RRO configuration
+TARGET_USES_RRO := true
+PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(COMMON_PATH)/overlay-system
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
