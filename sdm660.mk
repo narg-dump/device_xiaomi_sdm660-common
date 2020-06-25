21ef38bf1d206251e5d8aebba3a9d50bc648ce36
narjirolu125 <narjirolu@gmail.com>
2020-06-25 23:50:37 +0000

    sdm660-common: add sepolicy for XiaomiPart

--- sdm660.mk
+++ sdm660.mk
@@ -306,7 +306,7 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.sh \
     init.recovery.qcom.rc \
     init.target.rc \
-    init.xiaomi_parts.rc \
+    init.xiaomi.rc \
     init.performance.sdm660.rc \
     ueventd.qcom.rc
 
