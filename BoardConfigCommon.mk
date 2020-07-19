160faa5b7e97a56e45d6e470ba7a573f9712cf58
narjirolu125 <narjirolu@gmail.com>
2020-07-19 09:33:56 +0000

    sdm660-common: Set BOARD_SEPOLICY_VERS
    Signed-off-by: narjirolu125 <narjirolu@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -216,6 +216,7 @@ include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/public
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
+BOARD_SEPOLICY_VERS := 29.0
 
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
